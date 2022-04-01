89965863f90980aa3871945f278702a0818b52cb
김예진 <wlsdong0004@gmail.com>
2022-04-01 10:57:19 +0900

    [FEAT] Login 모달

--- sgnb/src/store/index.js
+++ sgnb/src/store/index.js
@@ -6,11 +6,15 @@ Vue.use(Vuex);
 export default new Vuex.Store({
   state: {
     isListShow: false,
+    isModalShow: false,
   },
   getters: {},
   mutations: {
     SET_TOGGLE_MENU_LIST(state, toggle) {
       state.isListShow = toggle;
+    },
+    SET_TOGGLE_LOGIN(state, toggle) {
+      state.isModalShow = toggle;
     },
   },
   actions: {},
