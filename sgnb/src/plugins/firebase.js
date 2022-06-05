15e38a744bbc331a6a7d8e5b55f48f11f48fd99a
김예진 <wlsdong0004@gmail.com>
2022-06-06 00:49:25 +0900

    [SETTING]

---
+++ sgnb/src/plugins/firebase.js
@@ -0,0 +1,9 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import firebaseConfig from "../../firebaseConfig";
+
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app);
+
+export { db };
